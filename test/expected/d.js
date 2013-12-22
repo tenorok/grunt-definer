c0c7c478eec00e89ad6ecb743cc9974f499d9b69
tenorok <mail@tenorok.ru>
2013-12-22 18:29:55 +0400

    Добавлены файлы правильных результатов тестов

---
+++ test/expected/d.js
@@ -0,0 +1,14 @@
+(function(global) {
+var a = (function () { return 'a'; }).call(global),
+b = (function (a){return a + 'b';}).call(global, a),
+c = (function (a, b) {
+    return a + b + 'c';
+}).call(global, a, b),
+d = (function (
+        a,
+        b,
+        c
+    ){
+        return a + b + 'c';
+    }).call(global, a, b, c);
+})(this);
